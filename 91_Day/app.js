c11fd60ff6e7d2aec8e8577692d37f58f64b00e1
Nihil <35506793+Nihilnia@users.noreply.github.com>
2021-07-07 02:27:37 +0300

    ready to go for typeScript

---
+++ 91_Day/app.js
@@ -0,0 +1,8 @@
+//* CommonJS Module Syntax */
+
+console.log("Hello, Gloria is here.");
+
+console.log(global);
+var loggerJS = require('./logger');
+loggerJS.sayHello("404");
+console.log(loggerJS.Gloria);
---
+++ 91_Day/logger.js
@@ -0,0 +1,17 @@
+var logger = "Looogger.asad";
+
+
+var Gloria = "projectGloria";
+
+function sayHello(name){
+    console.log(name);
+}
+
+
+module.exports = {
+    Gloria: Gloria,
+    sayHello: sayHello
+}
+
+
+//* logger.JS */
